8634fe8a40bea27f463ce19f384cd137d757625b
Kama <90838159+satcom-uhf@users.noreply.github.com>
2021-10-15 22:24:55 +0500

    let test it

--- owrx/controllers/websocket.py
+++ owrx/controllers/websocket.py
@@ -1,10 +1,13 @@
 from . import Controller
 from owrx.websocket import WebSocketConnection
 from owrx.connection import HandshakeMessageHandler
+import logging
 
+logger = logging.getLogger(__name__)
 
 class WebSocketController(Controller):
     def indexAction(self):
+        logger.debug("WEBSOOOKETTTT")
         conn = WebSocketConnection(self.handler, HandshakeMessageHandler())
         # enter read loop
         conn.handle()
